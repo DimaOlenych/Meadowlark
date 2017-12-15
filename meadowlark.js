8da0311e9cc5909f0547cc649ed448cb8af00768
DimaOlenych <olenychdima@gmail.com>
2017-12-16 01:22:32 +0200

    fixed

--- meadowlark.js
+++ meadowlark.js
@@ -2,38 +2,34 @@ var express = require('express');
 var app = express();
 
 // handlebars
-var handlebars = require('express-handlebars')
-    .create({ defaultLayout: 'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', handlebars);
+var handlebars = require('express-handlebars');
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
 // home page
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 // about page
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('O Meadowlark Travel');
+    res.render('about');
 });
 
 // page 404
-app.use(function(req, res) {
-    res.type('text/plain');
+app.use(function(req, res, next) {
     res.status(404);
-    res.send('404 - Не найдено');
+    res.render('404');
 });
 
 // page 500
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Ошибка сервера');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
