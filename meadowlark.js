e053120684d855654c0562bb90e2683c717cf46a
DimaOlenych <olenychdima@gmail.com>
2017-12-19 22:41:11 +0200

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js')
 
 // handlebars
 var handlebars = require('express-handlebars');
@@ -16,9 +17,7 @@ app.get('/', function(req, res) {
 
 // about page
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() *
-        fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // page 404
@@ -39,10 +38,10 @@ app.listen(app.get('port'), function() {
         app.get('port') + '; нажмите Ctrl+C для завершения.');
 });
 
-var fortunes = [
-    "Победи свои страхи, или они победят тебя.",
-    "Рекам нужны истоки.",
-    "Не бойся неведомого.",
-    "Тебя ждет приятный сюрприз.",
-    "Будь проще везде, где только можно."
-];
+// var fortunes = [
+//     "Победи свои страхи, или они победят тебя.",
+//     "Рекам нужны истоки.",
+//     "Не бойся неведомого.",
+//     "Тебя ждет приятный сюрприз.",
+//     "Будь проще везде, где только можно."
+// ];
